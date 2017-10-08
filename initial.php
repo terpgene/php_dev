7916a1d587aff37042fa5a44f9af18f290a10ede
Gene K. Essel <eugene2essel@gmail.com>
2017-10-07 21:17:37 -0400

    Connect to database

---
+++ initial.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+      <title>Test Results</title>
+
+  </head>
+
+  <body>
+    <?php
+      // Connect to Database
+      $hostname = 'localhost';
+      $username = 'sdev_owner';
+      $password = 'sdev300';
+      $dbase = 'sdev';
+
+
+      $conn = mysql_connect( $hostname, $username, $password, $dbase);
+      if (!$conn) {
+        die('Could not connect: '.mysql_error());
+      }
+      mysql_select_db($dbase,$conn) or die(mysql_error());
+      $lastname = $_POST["lastname"];
+      $firstname = $_POST["firstname"];
+      $course = $_POST["course"];
+
+
+     ?>
+
+  </body>
+</html>
