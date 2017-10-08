82d2f2d785eb33727b8690a6d8a8650bc0aa73ee
Gene K. Essel <eugene2essel@gmail.com>
2017-10-07 21:49:29 -0400

    First commit for search.php, includes text and submit

---
+++ search.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Testing SQL</title>
+  </head>
+  <body>
+  <h2> Find a Course by FacultyID </h2>
+  <form action="main.php" method="post">
+    Faculty ID: <input type="text" name="faculty_id"><br>
+    <input input="submit" value="Submit">
+  </form>
+  </body>
+  </head>
+</html>
