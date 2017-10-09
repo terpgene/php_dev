1324848392490291efadb6bb287da9b2af1193bd
Gene K. Essel <eugene2essel@gmail.com>
2017-10-08 20:40:37 -0400

    Add form to get faculty id

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+      <title>Faculty Database</title>
+  </head>
+
+  <body>
+    <?php echo "<h3 align='center'> Faculty Database</h3>" ?>
+
+    <form action="Search.php" method="post">
+        Faculty ID: <input type="text" name="faculty_id"><br>
+        <input type="submit" value="Submit">
+
+    </form>
+
+  </body>
+
+</html>
